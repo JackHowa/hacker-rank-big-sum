1902f8ef81976b2710563235732a01f7dc7082bc
jackhowa <johncarverhoward@gmail.com>
2017-08-08 15:03:56 -0500

    use enumerable inject to sum all in array

--- bigSum.rb
+++ bigSum.rb
@@ -1,7 +1,9 @@
 #!/bin/ruby
 
 def aVeryBigSum(n, ar)
-    # Complete this function
+  # Combines all elements of enum by applying a binary operation, specified by a block or a symbol that names a method or operator.
+  # index of zero 
+  ar.inject{|sum,x| sum + x }
 end
 
 n = gets.strip.to_i
