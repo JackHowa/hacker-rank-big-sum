ab45e1708fec17cac437ec5049014a93c7034abd
jackhowa <johncarverhoward@gmail.com>
2017-08-08 14:37:15 -0500

    Add the starter code, read the instructions

---
+++ bigSum.rb
@@ -0,0 +1,11 @@
+#!/bin/ruby
+
+def aVeryBigSum(n, ar)
+    # Complete this function
+end
+
+n = gets.strip.to_i
+ar = gets.strip
+ar = ar.split(' ').map(&:to_i)
+result = aVeryBigSum(n, ar)
+puts result;
